18b15380ec08f79e962ec39637aa085c41770e57
shepych <syndicating@yandex.ru>
2025-02-02 13:39:33 +0300

    Feat: доработка устройства

---
+++ config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "wifi_name";
+const char* password = "wifi_pass";
+
+#endif
--- sketch.ino
+++ sketch.ino
@@ -1,18 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
-const char* ssid = "wifi_name";
-const char* password = "wifi_pass";
+#include <RCSwitch.h> // Библиотека для работы с 433 МГц
+#include "config.h"   // Подключаем файл конфигурации
 
 WiFiClientSecure client;
+RCSwitch mySwitch = RCSwitch(); // Создаём объект для работы с приёмником
 
 const int relayPin = D1; // Пин, к которому подключено реле
+const int receiverPin = D2; // Пин, к которому подключён приёмник 433 МГц
+
+const int enterCode = 7203346;
+const int exitCode = 2991634;
+
+bool openRoom = false;
 
 void setup() {
-  pinMode(relayPin, OUTPUT);          
+  pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 
-  Serial.begin(9600);                  
+  Serial.begin(9600);
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting to WiFi");
@@ -21,37 +27,65 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
+
+  mySwitch.enableReceive(receiverPin); // Включаем приёмник на указанном пине
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) { 
-    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-    client->setInsecure();   
-    HTTPClient http; 
+  if (mySwitch.available()) {
+    unsigned long receivedValue = mySwitch.getReceivedValue(); // Получаем значение
 
-    http.begin(*client, "https://api.bigsports.ru/arduino/index.php");
-    int httpCode = http.GET();
 
-    if (httpCode > 0) { // Проверка наличия ответа от сервера
-      if (httpCode == HTTP_CODE_OK) {     
-        String payload = http.getString();
-        Serial.println(payload);
-        if(payload == "1") {
-          digitalWrite(relayPin, HIGH);
-        } else if (payload == "0") {
-          digitalWrite(relayPin, LOW);
+    // Получили сигнал
+    if (receivedValue != 0) {
+      if(receivedValue == enterCode) {
+        openRoom = true;
+        digitalWrite(relayPin, HIGH);
+        if (WiFi.status() == WL_CONNECTED) {
+          std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+          client->setInsecure();
+          HTTPClient http;
+
+          http.begin(*client, "https://jsonb.ru/meetingRoom/status_room_api.php?key=1");
+          int httpCode = http.GET();
+          if (httpCode > 0) {
+              // Проверяем код ответа
+              if (httpCode == HTTP_CODE_OK) {
+                  String payload = http.getString();  // Получаем ответ от сервера
+                  Serial.println("Ответ сервера: " + payload);
+              } else {
+                  Serial.printf("Ошибка HTTP: %s\n", http.errorToString(httpCode).c_str());
+              }
+          } else {
+              Serial.println("Ошибка подключения");
+          }
         }
-      } else {
-        Serial.printf("HTTP request failed with code: %d\n", httpCode);
+      } else if(receivedValue == exitCode) {
+        openRoom = false;
+        digitalWrite(relayPin, LOW);
+        if (WiFi.status() == WL_CONNECTED) {
+          std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+          client->setInsecure();
+          HTTPClient http;
+
+          http.begin(*client, "https://jsonb.ru/meetingRoom/status_room_api.php?key=0");
+          int httpCode = http.GET();
+          if (httpCode > 0) {
+              // Проверяем код ответа
+              if (httpCode == HTTP_CODE_OK) {
+                  String payload = http.getString();  // Получаем ответ от сервера
+                  Serial.println("Ответ сервера: " + payload);
+              } else {
+                  Serial.printf("Ошибка HTTP: %s\n", http.errorToString(httpCode).c_str());
+              }
+          } else {
+              Serial.println("Ошибка подключения");
+          }
+        }
       }
-    } else {
-      Serial.printf("HTTP request failed, error: %s\n", http.errorToString(httpCode).c_str());
+      Serial.print("\nReceived 433 MHz signal: ");
+      Serial.println(receivedValue);
+      mySwitch.resetAvailable(); // Сбрасываем состояние приемника
     }
-
-    http.end();
-  } else {
-    Serial.println("WiFi Disconnected");
   }
-
-  delay(1000); // Задержка 1 секунда
 }
